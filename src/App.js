f8df3136d41f6fd28b2df36db400431ecd3e9ff5
Michael <mikek222@gmail.com>
2025-11-06 13:34:14 -0500

    first changes, having issues with ready status

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./Start";
 
 const initialState = {
   questions: [],
@@ -27,7 +30,9 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
@@ -37,12 +42,13 @@ export default function App() {
   }, []);
 
   return (
-    <div className="app">
+    <div className='app'>
       <Header />
 
-      <Main className="main">
-        <p>1/15</p>
-        <p>question</p>
+      <Main className='main'>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
       </Main>
     </div>
   );
